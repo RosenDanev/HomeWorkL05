e10c45678c2bc32ca3fa07c559fafb00162d622a
RosenDanev <danev_r@abv.bg>
2023-01-11 11:47:00 +0200

    Method usage

---
+++ src/main/java/HomeWorkL07/Task4Version3.java
@@ -0,0 +1,17 @@
+package HomeWorkL07;
+
+public class Task4Version3 {
+    public static void main(String[] args) {
+        numbersToN(10);
+
+    }
+
+    public static void numbersToN(int number){
+        for (int i = 0; i <= number ; i++) {
+            System.out.println(i);
+
+        }
+
+
+    }
+}
